b0f6267aaaa8551b8634079f697a6914e8077fcd
DaveDLeague <DaveD@Leagues-iMac.attlocal.net>
2017-11-12 14:08:30 -0800

    initial

---
+++ src/Polymorph.java
@@ -0,0 +1,17 @@
+import java.awt.Graphics;
+
+public abstract class Polymorph {
+    int x;
+    int y;
+    
+    Polymorph(int x, int y){
+   	 this.x = x;
+   	 this.y = y;
+    }
+    
+    public void update(){
+   	 
+    }
+    
+    public abstract void draw(Graphics g);
+}
